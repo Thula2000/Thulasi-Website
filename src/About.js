1814ce41ea803723b81fc7f0ce11e55597f6978f
Thula2000 <65471017+Thula2000@users.noreply.github.com>
2020-06-05 18:35:14 -0400

    minor changes

---
+++ src/About.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function About() {
+  return <div>About me!</div>;
+}
+export default About;
---
+++ src/Videos.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Videos() {
+  return <div>Yay Videos!</div>;
+}
+export default Videos;
