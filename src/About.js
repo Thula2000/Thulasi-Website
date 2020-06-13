17fa779356619f5c0f19a7b5ba67143963d71996
Thula2000 <65471017+Thula2000@users.noreply.github.com>
2020-06-13 00:25:35 -0400

    aesthetic changes

--- src/About.js
+++ src/About.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./Heading";
-
+import Home from "./Home";
+import "./App.css";
 function About() {
+  const [content, setContent] = useState("about");
+  function clickedButton() {
+    setContent("home");
+  }
+  if (content === "home") {
+    return <Home />;
+  }
   return (
     <div className="background">
-      <div className="about">
-        <header className="aboutheader">
-          <h1 class="aboutheading">
-            Bookworm, animal lover, anime enthusiast & admirer of good music
-          </h1>
-          <p class="aboutparagraph">
-            I have always been fascinated in the medical field which led me to
-            pursue my undergraduate in biomedical engineering at Ryerson
-            University. As someone who loves to learn, the combination of the
-            core principles of science, medicine and engineering felt perfect.
-            Include: -what I offer to the world -future aspirations
-          </p>
-        </header>
-      </div>
+      <header className="aboutheader">
+        <Heading />
+        <h1 class="aboutheading">
+          Bookworm, animal lover, anime enthusiast & admirer of good music
+        </h1>
+        <p class="aboutparagraph">
+          I have always been fascinated in the medical field which led me to
+          pursue my undergraduate in biomedical engineering at Ryerson
+          University. As someone who loves to learn, the combination of the core
+          principles of science, medicine and engineering felt perfect. Include:
+          -what I offer to the world -future aspirations
+        </p>
+
+        <a class="app-link" onClick={() => clickedButton()}>
+          Home
+        </a>
+      </header>
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -4,43 +4,23 @@ import About from "./About";
 import Videos from "./Videos";
 import Home from "./Home";
 import "./App.css";
+
 function App() {
   return (
     <Router>
-      <div>
-        <div className="header-bar" style={{ display: "flex" }}>
-          <div style={{ width: 300 }} className="logo-container">
-            <img
-              src="PersonalLogo.png"
-              className="Logo"
-              style={{ width: "100%" }}
-            ></img>
-          </div>
-          <div className="flex-container links-container">
-            <div className="nav-link">
-              <Link to="/">My Homepage</Link>
-            </div>
-            <div className="nav-link">
-              <Link to="/about">About Me</Link>
-            </div>
-            <div className="nav-link">
-              <Link to="/v">Videos</Link>
-            </div>
-          </div>
-        </div>
-        <Switch>
-          <Route path="/about">
-            <About></About>
-          </Route>
-          <Route path="/v">
-            <Videos></Videos>
-          </Route>
-          <Route path="/">
-            <Home></Home>
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/about">
+          <About></About>
+        </Route>
+        <Route path="/v">
+          <Videos></Videos>
+        </Route>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
     </Router>
   );
 }
+
 export default App;
